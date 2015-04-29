a3d14545d5677f84d49346d0babfffd73db4fca5
Stephen Done <srdone@gmail.com>
2015-04-29 17:27:59 -0600

    clearing out practice - ready to build app

--- public/index.jsx
+++ public/index.jsx
@@ -1,7 +1,2 @@
-var React = require('react');
-var Hello = require('./components/Hello');
-var CommentBox = require('./components/CommentBox');
+import React from 'react';
 
-React.render(<Hello />, document.getElementById('content'));
-
-React.render(<CommentBox url="data.json" pollInterval={2000} />, document.getElementById('tutorial'));
