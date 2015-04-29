0fcd64040f9fe3f913e583518ee10bb51b98c113
Stephen Done <srdone@gmail.com>
2015-04-29 17:33:32 -0600

    valid TodoApp component

---
+++ public/components/TodoApp.jsx
@@ -0,0 +1,8 @@
+export default class TodoApp extends React.Component {
+
+  render() {
+    return <div>I'm the Todo App</div>
+  }
+
+}
+
--- public/index.jsx
+++ public/index.jsx
@@ -1,2 +1,4 @@
 import React from 'react';
+import TodoApp from './components/TodoApp';
 
+React.render(<TodoApp />, document.getElementById('app'));
