a7466c883b80f5241be9b389229c5cff0fea7e95
Stephen Done <srdone@gmail.com>
2015-04-28 20:10:39 -0600

    initial hello world server

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.use('/', express.static(__dirname + '/public/'));
+
+var port = 3000;
+
+var server = app.listen(port, function () {
+
+  var address = server.address().address;
+  var port = server.address().port;
+
+  console.log('Listening on http://%s:%s', address, port);
+});
