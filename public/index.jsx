eded33301cd704e8f81b4d38326aeec661386ab1
Stephen Done <srdone@gmail.com>
2015-04-30 00:06:53 -0600

    playing around with update state with new Checkbox component

--- public/index.jsx
+++ public/index.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
 import TodoApp from './components/TodoApp';
+import Checkbox from './components/Checkbox';
 
 
 React.render(<TodoApp url="http://localhost:8082"/>, document.getElementById('app'));
+
+var test = {
+  checked: true,
+  label: 'testing checkbox'
+};
+
+var update = function (checked) {
+  console.log('update called');
+  console.log('checked?:', checked);
+};
+
+React.render(<Checkbox checked={test.checked} update={update} label={test.label} />, document.getElementById('test'));
