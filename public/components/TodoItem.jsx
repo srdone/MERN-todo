80c5113f02cc2c2e136060a0193db63c9b253b62
Stephen Done <srdone@gmail.com>
2015-04-30 12:13:12 -0600

    added todo item wireframe

---
+++ public/components/TodoItem.jsx
@@ -0,0 +1,9 @@
+export default class TodoItem extends React.Component {
+
+  render() {
+    return (
+      <div>I'm a todoItem</div>
+    )
+  }
+
+}
--- public/components/TodosList.jsx
+++ public/components/TodosList.jsx
@@ -1,9 +1,12 @@
+import TodoItem from './TodoItem';
+
 export default class TodosList extends React.Component {
 
   render() {
     return (
       <div>
         <div>I'm the Todos List</div>
+        <TodoItem />
       </div>
     )
   }
