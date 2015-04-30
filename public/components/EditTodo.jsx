5413e47144832a54742d52a934da57f09dd93c11
Stephen Done <srdone@gmail.com>
2015-04-30 17:45:48 -0600

    working lookup of individual todo

--- public/components/EditTodo.jsx
+++ public/components/EditTodo.jsx
@@ -1,13 +1,34 @@
 export default class EditTodo extends React.Component {
 
+  constructor() {
+    super();
+    this.state = {todo: {}};
+  }
+
+  componentDidMount() {
+    var { router } = this.context;
+    var todoId = router.getCurrentParams().todoId;
+    $.ajax({
+      url: 'http://localhost:8082' + '/todos/' + todoId,
+      dataType: 'json',
+      crossDomain: true,
+      success: (todo) => this.setState({todo: todo}),
+      error: (xhr, status, err) => console.error('http://localhost:8082', status, err.toString())
+    })
+  }
+
   render() {
 
     return (
       <div>
-        I'm The Todo To Edit
+        {this.state.todo}
       </div>
     )
 
   }
   
 }
+
+EditTodo.contextTypes = {
+  router: React.PropTypes.func
+};
