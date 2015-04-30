89309d0784678cf0f3f4a1154da53f72250300a0
Stephen Done <srdone@gmail.com>
2015-04-29 18:21:06 -0600

    added button to make editable

--- public/components/MakeEditable.jsx
+++ public/components/MakeEditable.jsx
@@ -2,14 +2,27 @@ export default class MakeEditable extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {editable: false};
+    this._toggleEditable = this._toggleEditable.bind(this);
+  }
+
+  _toggleEditable() {
+    this.setState({editable: true});
   }
 
   render() {
 
-    return <div>
-      Made Editable
+    if (this.state.editable) {
+      return <div>
+        Made Editable
         {this.props.children}
-    </div>
+      </div>
+    } else {
+      return <div>
+          {this.props.children}
+          <button onClick={this._toggleEditable}>Make Editable</button>
+        </div>
+    }
   }
 
 }
