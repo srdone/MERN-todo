6ee05b7c3cccf20351ad85f18fdcd64c5bd74bf7
Stephen Done <srdone@gmail.com>
2015-04-30 22:50:02 -0600

    separate out routes

--- public/index.jsx
+++ public/index.jsx
@@ -1,19 +1,6 @@
 import React from 'react';
 import Router from 'react-router';
-import TodoApp from './components/TodoApp';
-import TodosList from './components/TodosList';
-import EditTodo from './components/EditTodo';
-
-var DefaultRoute = Router.DefaultRoute;
-var Route = Router.Route;
-var RouteHandler = Router.RouteHandler;
-
-var routes = (
-  <Route name="app" path="/" handler={TodoApp}>
-    <DefaultRoute handler={TodosList} />
-    <Route name="edit" path="edit/:todoId" handler={EditTodo} />
-  </Route>
-);
+import { routes } from './routes';
 
 Router.run(routes, (Handler) => {
   React.render(<Handler />, document.body);
---
+++ public/routes.jsx
@@ -0,0 +1,19 @@
+import Router from 'react-router';
+import TodoApp from './components/TodoApp';
+import TodosList from './components/TodosList';
+import EditTodo from './components/EditTodo';
+
+var DefaultRoute = Router.DefaultRoute;
+var Route = Router.Route;
+var RouteHandler = Router.RouteHandler;
+
+var routes = (
+  <Route name="app" path="/" handler={TodoApp}>
+    <DefaultRoute handler={TodosList} />
+    <Route name="edit" path="edit/:todoId" handler={EditTodo} />
+  </Route>
+);
+
+module.exports = {
+  routes: routes
+};
